e7557a404bf4dfde50163554a26ffe1a8aa98c79
charan_7 <neelampallicharanbalaji14@gmail.com>
2026-03-11 19:51:24 +0530

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./sections/navbar.jsx";
+import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
